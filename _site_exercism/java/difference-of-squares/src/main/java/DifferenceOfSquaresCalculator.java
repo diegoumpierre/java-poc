ebf6b4fb9d0211eca276fa185695d11d3ec1fef3
dumpierre <dumpierre@lendingclub.com>
2024-02-22 11:46:21 -0300

    add the difference of square

--- _site_exercism/java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ _site_exercism/java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,35 @@
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int sum = 0;
+        for (int i=0; i <= input;i++){
+            sum +=i;
+        }
+        return (int) Math.pow(sum,2);
     }
 
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        int sum = 0;
+        for (int i=0; i <= input;i++){
+            sum +=(int) Math.pow(i,2);
+        }
+        return sum;
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return (computeSquareOfSumTo(input) - computeSumOfSquaresTo(input));
     }
 
 }
+/*
+The square of the sum of the first ten natural numbers is
+(1 + 2 + ... + 10)² = 55² = 3025.
+
+The sum of the squares of the first ten natural numbers is
+1² + 2² + ... + 10² = 385.
+
+Hence the difference between the square of the sum of the first ten natural numbers and the sum of the
+squares of the first ten natural numbers is 3025 - 385 = 2640.
+
+ */
--- _site_exercism/java/grains/src/main/java/Grains.java
+++ _site_exercism/java/grains/src/main/java/Grains.java
@@ -3,11 +3,25 @@ import java.math.BigInteger;
 class Grains {
 
     BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (square < 1 || square >64)
+            throw new IllegalArgumentException("square must be between 1 and 64");
+
+        BigInteger totalGrains = BigInteger.ZERO;
+        for (int i=1;i<=square;i++){
+            if (totalGrains.compareTo(BigInteger.ZERO) != 0)
+                totalGrains = totalGrains.multiply(BigInteger.valueOf(2));
+            else
+                totalGrains = totalGrains.add(BigInteger.ONE);
+        }
+        return totalGrains;
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        BigInteger totalGrainsOnBoard = BigInteger.ZERO;
+        for (int i=1;i<65;i++){
+            totalGrainsOnBoard = totalGrainsOnBoard.add(grainsOnSquare(i));
+        }
+        return totalGrainsOnBoard;
     }
 
 }
