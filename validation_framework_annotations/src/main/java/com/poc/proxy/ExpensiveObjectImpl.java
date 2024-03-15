5eda95785c10a7473b7c17988d0be01a1c6d3f51
dumpierre <dumpierre@lendingclub.com>
2024-03-15 16:46:04 -0300

    add the proxy concept

---
+++ validation_framework_annotations/src/main/java/com/poc/proxy/ExpensiveObjectImpl.java
@@ -0,0 +1,22 @@
+package com.poc.proxy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExpensiveObjectImpl implements ExpensiveObject {
+
+    //private final log = org.apache.log4j.Logger.getLogger(ExpensiveObjectImpl.class);
+    public static final Logger LOGGER = LoggerFactory.getLogger(ExpensiveObjectImpl.class);
+
+
+    @Override
+    public void process() {
+        LOGGER.info("processing complete.");
+    }
+
+    private void heavyInitialConfiguration() {
+        LOGGER.info("Loading initial configuration...");
+    }
+
+
+}
