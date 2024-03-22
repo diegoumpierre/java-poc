a1e1a7d7cc994e8ac4f72766d3650503bd401d52
dumpierre <dumpierre@lendingclub.com>
2024-03-22 19:41:47 -0300

    first validate

--- validation_framework_annotations/src/test/java/com/poc/domain/validation/AnnotationValidationTest.java
+++ validation_framework_annotations/src/test/java/com/poc/domain/validation/AnnotationValidationTest.java
@@ -18,54 +18,5 @@ class AnnotationValidationTest {
 
         //expects a exception
 
-
-/*
-        public void readClass(Object object) {
-            String className = object.getClass().getName();
-
-            if (classFieldMap.containsKey(className)) return;
-
-            Field[] fields = object.getClass().getDeclaredFields();
-            List<CustomMapperFieldDetailModel> attributes = new ArrayList<>();
-            try {
-                for (Field field : fields) {
-                    attributes.add(
-                            new CustomMapperFieldDetailModel(
-                                    field.getName(),
-                                    field.getType().equals(boolean.class)
-                            )
-                    );
-
-                    //check if the field have the alias
-                    List<String> fieldAlias = splitAliasNames(field.getAnnotation(Alias.class));
-                    if (fieldAlias != null && !fieldAlias.isEmpty()) {
-                        fieldAlias.stream().forEach((alias) -> {
-                                    attributes.add(
-                                            new CustomMapperFieldDetailModel(
-                                                    alias,
-                                                    field.getType().equals(boolean.class),
-                                                    field.getName()
-                                            )
-                                    );
-                                }
-                        );
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            classFieldMap.put(className, attributes);
-        }
-
-        List<String> splitAliasNames(Alias alias) {
-            if (alias == null || alias.otherFieldNames() == null) return Collections.emptyList();
-            return Arrays.asList(alias.otherFieldNames().split(","));
-        }
-
-
-    }
-
- */
-
     }
 }
