96d4b6f6ae3d98d471e04f7627a5ddde01e367a9
Diego Umpierre <diego@umpierre.com.br>
2024-04-17 23:21:44 -0300

    start add the tests

---
+++ fileshare_System/src/test/java/com/poc/directory/TreeDirectoryTest.java
@@ -0,0 +1,24 @@
+package com.poc.directory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TreeDirectoryTest {
+
+    @Test
+    void listAllFiles() {
+    }
+
+    @Test
+    void isFileExists() {
+    }
+
+    @Test
+    void createNewFile() {
+    }
+
+    @Test
+    void removeFile() {
+    }
+}
