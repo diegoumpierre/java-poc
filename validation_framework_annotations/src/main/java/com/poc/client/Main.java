4597268bb82990e0dcbca53a98a73d75f90cfa93
dumpierre <dumpierre@lendingclub.com>
2024-05-17 20:27:20 -0300

    Revert "test the client"
    This reverts commit ef8ad7907110fff5761d7bb344e6597d1ba91f24.

--- validation_framework_annotations/src/main/java/com/poc/client/Main.java
+++ validation_framework_annotations/src/main/java/com/poc/client/Main.java
@@ -2,8 +2,6 @@ package com.poc.client;
 
 import com.poc.client.client1.domain.Person;
 import com.poc.client.client1.service.PersonServiceHelper;
-import com.poc.client.client2.domain.Car;
-import com.poc.client.client2.service.CarServiceHelper;
 
 public class Main {
 
@@ -16,9 +14,8 @@ public class Main {
         personServiceHelper.doSomeProcess(person);
 
         //client2
-        Car car = new Car("Tubarao", 10);
-        CarServiceHelper carServiceHelper = new CarServiceHelper();
-        carServiceHelper.howManyProcess(car);
+
+
 
     }
 
