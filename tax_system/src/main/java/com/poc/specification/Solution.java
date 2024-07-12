cc2c2a39e3b33030e5603c36ab8a1fefdfebf24f
dumpierre <dumpierre@lendingclub.com>
2024-07-11 22:48:19 -0300

    wrong solution

--- tax_system/src/main/java/com/poc/specification/Solution.java
+++ tax_system/src/main/java/com/poc/specification/Solution.java
@@ -12,17 +12,4 @@ public class Solution {
     private Map<Long, Tax> listMap = new HashMap<>();
 
 
-
-
-
-    public Tax searchByYear(Long year){
-        return listMap.getOrDefault(year,new Tax());
-    }
-
-    public void removeTax(Long year, Tax.MonthEnum month ){
-
-        Tax tax = listMap.get(year);
-        tax.getApr().equals(month);
-        listMap.remove(year);
-    }
 }
