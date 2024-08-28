0001bcc613138df4aa647684615e681909a8f90a
dumpierre <dumpierre@lendingclub.com>
2024-08-28 08:55:28 -0300

    moving the logic to the service

--- springsecurity/src/main/java/com/poc/springsecurity/controller/TweetController.java
+++ springsecurity/src/main/java/com/poc/springsecurity/controller/TweetController.java
@@ -2,22 +2,11 @@ package com.poc.springsecurity.controller;
 
 import com.poc.springsecurity.controller.dto.CreateTweetDto;
 import com.poc.springsecurity.controller.dto.FeedDto;
-import com.poc.springsecurity.controller.dto.FeedItemDto;
-import com.poc.springsecurity.entity.Role;
-import com.poc.springsecurity.entity.Tweet;
-import com.poc.springsecurity.repository.TweetRepository;
-import com.poc.springsecurity.repository.UserRepository;
 import com.poc.springsecurity.service.TweetService;
-import org.apache.coyote.Response;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.UUID;
 
 @RestController
 public class TweetController {
@@ -31,60 +20,24 @@ public class TweetController {
     @GetMapping("/feed")
     public ResponseEntity<FeedDto> feed(@RequestParam(value = "page", defaultValue = "0") int page,
                                             @RequestParam(value = "pageSize", defaultValue = "10") int pageSize){
-        var tweets = tweetRepository.findAll(
-                PageRequest.of(page,pageSize, Sort.Direction.DESC,"createdTimestamp"))
-                .map(tweet ->
-                        new FeedItemDto(
-                                tweet.getTweetId(),
-                                tweet.getContent(),
-                                tweet.getUser().getUsername()
-                        ));
-
-        return ResponseEntity.ok(new FeedDto(
-                tweets.getContent(),
-                page,
-                pageSize,
-                tweets.getTotalPages(),
-                tweets.getTotalElements()
-        ));
+        return ResponseEntity.ok(tweetService.findAll(page,pageSize));
     }
 
     @PostMapping("/tweets")
     public ResponseEntity<Void> createTweet(@RequestBody CreateTweetDto dto,
-                                            JwtAuthenticationToken token) {
-
-        var user = userRepository.findById(UUID.fromString(token.getName()));
-
-        var tweet = new Tweet();
-        tweet.setUser(user.get());
-        tweet.setContent(dto.content());
-        tweetRepository.save(tweet);
-
+                                            JwtAuthenticationToken token) throws Exception {
+        tweetService.createNew(token.getName(), dto.content());
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("tweets/{id}")
     public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,
-                                            JwtAuthenticationToken token){
-
-        var userIdFromToken = UUID.fromString(token.getName());
-
-        var user = userRepository.findById(userIdFromToken);
-        var tweet = tweetRepository.findById(tweetId)
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-        var isAdmin = user.get().getRoles()
-                .stream()
-                .anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
-
-        if (isAdmin || tweet.getUser().getUserId().equals(userIdFromToken)){
-            tweetRepository.deleteById(tweetId);
+                                            JwtAuthenticationToken token) {
+        if (tweetService.deleteTweet(token.getName(), tweetId)) {
+            return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
 
-            return ResponseEntity.ok().build();
-        }
-
-
+    }
 }
