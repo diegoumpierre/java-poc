a53f911fcb4401678fac0f613786889b0bef469b
dumpierre <dumpierre@lendingclub.com>
2024-10-27 23:35:59 -0300

    start implementing the adapter

---
+++ LoggerBuilder/src/main/java/com/poc/logger/LoggerDBImpl.java
@@ -0,0 +1,13 @@
+package com.poc.logger;
+
+public class LoggerDBImpl implements LoggerInterface{
+    @Override
+    public void warning(Exception error) {
+
+    }
+
+    @Override
+    public void error(Exception error) {
+
+    }
+}
