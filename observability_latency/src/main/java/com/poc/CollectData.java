871d4c19fb2244aed61f1aae2843d628fe7c7a78
dumpierre <dumpierre@lendingclub.com>
2024-11-15 10:59:53 -0800

    add the some logic to the poc

---
+++ observability_latency/src/main/java/com/poc/CollectData.java
@@ -0,0 +1,13 @@
+package com.poc;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class CollectData {
+
+    public static HashMap<String, List<DataCollected>> dataCollectedMap;
+
+
+
+
+}
