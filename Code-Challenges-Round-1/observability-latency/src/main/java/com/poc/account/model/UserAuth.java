5eccd806e5bc67cff217c62387914fa670edf3b9
dumpierre <dumpierre@lendingclub.com>
2024-11-22 05:34:56 -0800

    WIP

---
+++ Code-Challenges-Round-1/observability-latency/src/main/java/com/poc/account/model/UserAuth.java
@@ -0,0 +1,41 @@
+package com.poc.account.model;
+
+public class UserAuth {
+
+    private String nome;
+    private String email;
+    private int age;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
