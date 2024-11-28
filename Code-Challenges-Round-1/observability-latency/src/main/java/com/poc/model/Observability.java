b2249f7725232def2452cdefd36f8573dfddec1e
dumpierre <dumpierre@lendingclub.com>
2024-11-27 19:05:16 -0800

    WIP - improve the collector

---
+++ Code-Challenges-Round-1/observability-latency/src/main/java/com/poc/model/Observability.java
@@ -0,0 +1,13 @@
+package com.poc.model;
+
+public class Observability {
+
+    private int process;
+    private int success;
+    private int error;
+    private long start;
+    private long end;
+
+
+
+}
