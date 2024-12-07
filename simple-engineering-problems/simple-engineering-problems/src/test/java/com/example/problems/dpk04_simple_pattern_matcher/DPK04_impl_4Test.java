ffeba48a88bec9abc9b7dbc95f781eb2bafcb468
dumpierre <dumpierre@lendingclub.com>
2024-12-07 12:42:48 -0300

    WIP finish problem 4

---
+++ simple-engineering-problems/simple-engineering-problems/src/test/java/com/example/problems/dpk04_simple_pattern_matcher/DPK04_impl_4Test.java
@@ -0,0 +1,18 @@
+package com.example.problems.dpk04_simple_pattern_matcher;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DPK04_impl_4Test {
+
+    @Test
+    void getUsaShouldSuccess(){
+        assertEquals("English",DPK04_impl_4.pattern_matcher("Usa"));
+    }
+
+    @Test
+    void getUsaEnumShouldSuccess(){
+        assertEquals("English",DPK04_impl_4.pattern_matcher_enum("Usa"));
+    }
+}
