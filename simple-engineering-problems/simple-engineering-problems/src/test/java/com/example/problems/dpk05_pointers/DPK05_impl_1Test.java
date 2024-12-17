6ca4dc025846ee56313fd7de898db52c4fe62ba2
dumpierre <dumpierre@lendingclub.com>
2024-12-17 15:37:31 -0300

    Wip problem 5

--- simple-engineering-problems/simple-engineering-problems/src/test/java/com/example/problems/dpk05_pointers/DPK05_impl_1Test.java
+++ simple-engineering-problems/simple-engineering-problems/src/test/java/com/example/problems/dpk05_pointers/DPK05_impl_1Test.java
@@ -2,7 +2,12 @@ package com.example.problems.dpk05_pointers;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class DPK05_impl_1Test {
 
@@ -15,6 +20,27 @@ class DPK05_impl_1Test {
         assertEquals(90, dpk05Impl1.getPower("Paul"));
         assertEquals(80, dpk05Impl1.getPower("George"));
         assertEquals(70, dpk05Impl1.getPower("Ringo"));
+        assertEquals(-1, dpk05Impl1.getPower("Diego"));
+    }
+
+    @Test
+    void takeThepowerfullOneShouldBeSucess(){
+        assertEquals("John", dpk05Impl1.getPowerFullOne("George","John"));
+    }
+
+    @Test
+    void playWithLeaderboardShouldBeSuccess(){
+
+        assertEquals("John", dpk05Impl1.play("John", "Paul"));
+        Map<String, Integer> leaderboardExpected1 = Map.of(
+                "John",10,
+                "Paul", -5,
+                "George", 0,
+                "Ringo", 0
+        );
+        //assertThat(dpk05Impl1.getLeaderboard(), is(leaderboardExpected1));
+
+
     }
 
 }
