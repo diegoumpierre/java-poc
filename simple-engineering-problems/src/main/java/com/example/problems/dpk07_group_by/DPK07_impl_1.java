b501e38da63e94a5252c42470f2f927d297672c4
dumpierre <dumpierre@lendingclub.com>
2024-12-19 13:52:14 -0300

    finish the DPK07

---
+++ simple-engineering-problems/src/main/java/com/example/problems/dpk07_group_by/DPK07_impl_1.java
@@ -0,0 +1,29 @@
+package com.example.problems.dpk07_group_by;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DPK07_impl_1 {
+
+    public static List<List<Object>> group_by(List<Object> inputList, int size) {
+        List<List<Object>> result = new ArrayList<>();
+
+        List<Object> spot = new ArrayList<>();
+        int sizeCount = 0;
+        for (Object item :inputList){
+            if (sizeCount < size){
+                spot.add(item);
+                sizeCount++;
+            }else{
+                result.add(spot);
+                spot = new ArrayList<>();
+                spot.add(item);
+                sizeCount = 1;
+            }
+        }
+        result.add(spot);
+        return result;
+    }
+
+
+}
