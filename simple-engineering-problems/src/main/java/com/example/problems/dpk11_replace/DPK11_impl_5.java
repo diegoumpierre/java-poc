fae4a3e625da093ba7f2c58c6696a9f9a4c93bf7
Diego Umpierre <diego@umpierre.com.br>
2024-12-23 01:03:54 -0300

    finish DPK11

---
+++ simple-engineering-problems/src/main/java/com/example/problems/dpk11_replace/DPK11_impl_5.java
@@ -0,0 +1,24 @@
+package com.example.problems.dpk11_replace;
+
+public class DPK11_impl_5 {
+    public static String replace(String input, String token, String newToken) {
+        String result = "";
+        boolean replace = false;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 0; j < token.length(); j++) {
+                replace = true;
+                if ((i + j) < input.length() && input.charAt(i + j) != token.charAt(j)) {
+                    replace = false;
+                    break;
+                }
+            }
+            if (replace) {
+                result += newToken;
+                i += token.length() - 1;
+            } else {
+                result += input.charAt(i);
+            }
+        }
+        return result;
+    }
+}
---
+++ simple-engineering-problems/src/test/java/com/example/problems/dpk11_replace/DPK11_impl_5Test.java
@@ -0,0 +1,27 @@
+package com.example.problems.dpk11_replace;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DPK11_impl_5Test {
+
+    @Test
+    void replaceShouldBeSuccess() {
+        String expected = "Hello-World-How-Are-You";
+        String input = "Hello,World,How,Are,You";
+        String token = ",";
+        String newToken = "-";
+        assertEquals(expected,DPK11_impl_5.replace(input,token,newToken));
+    }
+
+    @Test
+    void replace2ShouldBeSuccess() {
+        String expected = "Hello-How,Are,You";
+        String input = "Hello,World,How,Are,You";
+        String token = ",World,";
+        String newToken = "-";
+        assertEquals(expected,DPK11_impl_5.replace(input,token,newToken));
+    }
+
+}
