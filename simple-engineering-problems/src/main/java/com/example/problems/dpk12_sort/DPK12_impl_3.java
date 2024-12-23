b1b6385b5f4f19797174135c0db85747cdd24cef
dumpierre <dumpierre@lendingclub.com>
2024-12-23 12:32:48 -0300

    DPK12 finished

---
+++ simple-engineering-problems/src/main/java/com/example/problems/dpk12_sort/DPK12_impl_3.java
@@ -0,0 +1,25 @@
+package com.example.problems.dpk12_sort;
+
+public class DPK12_impl_3 {
+
+    public static int[] bubble_sort(int[] input) {
+        int n = input.length;
+        int temp;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (input[j] > input[j + 1]) {
+                    swapped = true;
+                    temp = input[j];
+                    input[j] = input[j + 1];
+                    input[j + 1] = temp;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+        return input;
+    }
+}
---
+++ simple-engineering-problems/src/test/java/com/example/problems/dpk12_sort/DPK12_impl_8Test.java
@@ -0,0 +1,16 @@
+package com.example.problems.dpk12_sort;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DPK12_impl_8Test {
+
+    @Test
+    void sortShouldBeSuccess() {
+        int[] expected = {1, 2, 3, 4, 5};
+        int[] input = {5, 4, 3, 2, 1};
+        assertArrayEquals(expected, DPK12_impl_8.bubble_sort(input));
+    }
+
+}
