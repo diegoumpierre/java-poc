50d97110754d59e9437b6dd7ece35caf94009cc9
dumpierre <dumpierre@lendingclub.com>
2024-12-28 20:23:03 -0300

    WIP DPK14

--- simple-engineering-problems/src/test/java/com/example/problems/dpk14_2d_walk/DPK14_impl_2Test.java
+++ simple-engineering-problems/src/test/java/com/example/problems/dpk14_2d_walk/DPK14_impl_2Test.java
@@ -1,28 +1,22 @@
 package com.example.problems.dpk14_2d_walk;
 
-
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-class DPK14_impl_1Test {
+class DPK14_impl_2Test {
 
     @Test
-    void moveShouldBeSuccess() {
-
+    void shouldBeSuccess(){
         String[][] grid = {
                 {"Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"},
                 {"Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"}
         };
-
-        int[] initialPos = {0, 0};
+        int[] initialPos = {0,0};
         String[] movements = {"up", "left", "down", "right"};
         String[] expected = {"Ken", "M.Bison", "Vega"};
-        String[] result = DPK14_impl_1.move(grid, initialPos, movements).toArray(new String[0]);
+        String[] result = DPK14_impl_2.move(grid,initialPos,movements);
 
         assertArrayEquals(expected, result);
-
     }
-
-
 }
