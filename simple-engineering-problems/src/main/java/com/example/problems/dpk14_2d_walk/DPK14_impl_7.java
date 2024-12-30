7b4615a64939ced85e93397a8ac8613609890805
dumpierre <dumpierre@lendingclub.com>
2024-12-30 11:54:05 -0300

    Finish DPK14

---
+++ simple-engineering-problems/src/main/java/com/example/problems/dpk14_2d_walk/DPK14_impl_7.java
@@ -0,0 +1,54 @@
+package com.example.problems.dpk14_2d_walk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DPK14_impl_7 {
+    public static String[] move(String[][] grid, int[] initPos, String[] movements) {
+        List<String> deleteItems = new ArrayList<>();
+        int x = initPos[0], y = initPos[1];
+        String currentValue;
+        for (String move : movements) {
+            currentValue = grid[x][y];
+            grid[x][y] = "";
+            switch (move) {
+                case "up":
+                    if (x + 1 > 1) {
+                        x = 0;
+                    } else {
+                        x = 1;
+                    }
+                    break;
+                case "down":
+                    if (x - 1 < 0) {
+                        x = 1;
+                    } else {
+                        x = 0;
+                    }
+                    break;
+                case "left":
+                    if (y - 1 < 0) {
+                        y = 5;
+                    } else {
+                        y = y - 1;
+                    }
+                    break;
+                case "right":
+                    if (y + 1 > 5) {
+                        y = 0;
+                    } else {
+                        y = y + 1;
+                    }
+                    break;
+            }
+
+
+            if (!String.valueOf(grid[x][y]).isEmpty()) {
+                deleteItems.add(grid[x][y]);
+            }
+            grid[x][y] = currentValue;
+        }
+        return deleteItems.toArray(new String[0]);
+
+    }
+}
---
+++ simple-engineering-problems/src/test/java/com/example/problems/dpk14_2d_walk/DPK14_impl_7Test.java
@@ -0,0 +1,21 @@
+package com.example.problems.dpk14_2d_walk;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class DPK14_impl_7Test {
+    @Test
+    void moveShouldBeSuccess(){
+        String[][] grid = {
+                {"Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"},
+                {"Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"}
+        };
+        int[] initPos = {0, 0};
+        String[] movements = {"up", "left", "down", "right"};
+        String[] expected = {"Ken", "M.Bison", "Vega"};
+        String[] result = DPK14_impl_7.move(grid, initPos, movements);
+        assertArrayEquals(expected,result);
+    }
+
+}
