f61e06e51427573dc35c6c1ee43923ffc1447889
dumpierre <dumpierre@lendingclub.com>
2025-01-07 23:56:47 -0300

    WIP DPK15 finish the second implementation

--- simple-engineering-problems/src/test/java/com/example/problems/dpk15_oop_with_classes/DPK15_impl_2Test.java
+++ simple-engineering-problems/src/test/java/com/example/problems/dpk15_oop_with_classes/DPK15_impl_2Test.java
@@ -53,8 +53,108 @@ class DPK15_impl_2Test {
         person2.addFriend("Paul");
 
         assertEquals(1, person2.getFriends().size());
-        assertEquals("Paul", person2.getFriends().get(0));
+        assertEquals("Paul", person2.getFriends().get(0).getName());
     }
 
+    @Test
+    void personRemoveFriendsShouldSuccess(){
+        Person2 person2 = new Person2("John", 30);
+        person2.addFriend("Paul");
+        person2.addFriend("George");
+        person2.addFriend("Ringo");
+
+        List<Friend2> expectedFriends = new ArrayList<>();
+        expectedFriends.add(new Friend2("Paul"));
+        expectedFriends.add(new Friend2("Ringo"));
+
+        person2.removeFriend("George");
+        assertEquals(2, person2.getFriends().size());
+        assertEquals(expectedFriends.get(0).getName(), person2.getFriends().get(0).getName());
+    }
+
+
+    @Test
+    void moreFriendsShouldBeSuccess() {
+        List<Person2> person2List = new ArrayList<>();
+
+        Person2 person2 = new Person2("John", 30);
+        person2.addFriend("Paul");
+        person2.addFriend("George");
+        person2List.add(person2);
+
+        person2 = new Person2("Ringo", 89);
+        person2.addFriend("Paul");
+        person2.addFriend("George");
+        person2.addFriend("Ringo");
+        person2.addFriend("Ringo1");
+        person2.addFriend("Ringo2");
+        person2.addFriend("Ringo3");
+        person2List.add(person2);
+
+        person2 = new Person2("John1", 67);
+        person2.addFriend("Paul");
+        person2List.add(person2);
+
+
+        DPK15_impl_2 dpk15Impl2 = new DPK15_impl_2();
+        assertEquals("Ringo", dpk15Impl2.moreFriends(person2List));
+    }
+
+
+    @Test
+    void lessFriendsShouldBeSuccess() {
+
+        List<Person2> person2List = new ArrayList<>();
+
+        Person2 person2 = new Person2("John", 30);
+        person2.addFriend("Paul");
+        person2.addFriend("George");
+        person2List.add(person2);
+
+        person2 = new Person2("Ringo", 89);
+        person2.addFriend("Paul");
+        person2.addFriend("George");
+        person2.addFriend("Ringo2");
+        person2.addFriend("Ringo");
+        person2.addFriend("Ringo1");
+        person2.addFriend("Ringo3");
+        person2List.add(person2);
+
+        person2 = new Person2("John1", 67);
+        person2.addFriend("Paul");
+        person2List.add(person2);
+
+
+        DPK15_impl_2 dpk15Impl2 = new DPK15_impl_2();
+        assertEquals("John1", dpk15Impl2.lessFriends(person2List));
+    }
+
+    @Test
+    void oldestFriendsShouldBeSuccess() {
+
+        List<Person2> person2List = new ArrayList<>();
+
+        Person2 person2 = new Person2("John", 30);
+        person2.addFriend("Paul",60);
+        person2.addFriend("George",61);
+        person2List.add(person2);
+
+        person2 = new Person2("Ringo", 89);
+        person2.addFriend("Paul",69);
+        person2.addFriend("George", 70);
+        person2.addFriend("Ringo",10);
+        person2.addFriend("Ringo1",62);
+        person2.addFriend("Ringo2",64);
+        person2.addFriend("Ringo3",62);
+        person2List.add(person2);
+
+        person2 = new Person2("John1", 67);
+        person2.addFriend("Paul",20);
+        person2List.add(person2);
+
+
+        DPK15_impl_2 dpk15Impl2 = new DPK15_impl_2();
+        assertEquals("George", dpk15Impl2.oldestFriends(person2List));
+    }
 
 }
