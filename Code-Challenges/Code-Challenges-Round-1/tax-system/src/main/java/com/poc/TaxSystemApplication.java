f0e3b69329de8fd3a62f186cabac038551ac90b2
dumpierre <dumpierre@lendingclub.com>
2025-01-29 18:52:36 -0300

    WIP poc

---
+++ Code-Challenges/Code-Challenges-Round-1/tax-system/src/main/java/com/poc/TaxSystemApplication.java
@@ -0,0 +1,14 @@
+package com.poc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxSystemApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(TaxSystemApplication.class, args);
+    }
+
+}
