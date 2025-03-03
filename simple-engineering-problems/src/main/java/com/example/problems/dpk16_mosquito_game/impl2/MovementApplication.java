01310d959bae23ce36e1e2a2907f6ea9116c2549
dumpierre <dumpierre@lendingclub.com>
2025-03-03 19:56:58 -0300

    WIP DPK16 - delete

--- simple-engineering-problems/src/main/java/com/example/problems/dpk16_mosquito_game/impl2/MovementApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.problems.dpk16_mosquito_game.impl2;
-
-import com.example.problems.dpk16_mosquito_game.impl1.Game;
-
-interface MovementApplication {
-
-    int[] execute(int[] position);
-
-}
