3c8f19c66945b77a3b0e425a14fdd9a9eda0cba8
Diego Umpierre <diego@umpierre.com.br>
2025-03-15 18:46:57 -0300

    WIP impl2 finishing the mosquito

--- simple-engineering-problems/src/main/java/com/example/problems/dpk16_mosquito_game/impl2/Mosquito.java
+++ simple-engineering-problems/src/main/java/com/example/problems/dpk16_mosquito_game/impl2/Mosquito.java
@@ -1,4 +1,37 @@
 package com.example.problems.dpk16_mosquito_game.impl2;
 
+import java.util.Random;
+
 public class Mosquito {
+
+
+    private Random random;
+    private int[] position;
+    public int moves = 0;
+    private int gameRow, gameColumn;
+
+    public Mosquito(Random random, int[] position, int gameRow, int gameColumn) {
+        this.random = random;
+        this.position = position;
+        this.gameRow = gameRow;
+        this.gameColumn = gameColumn;
+    }
+
+    public int[] getPosition(){
+        return this.position;
+    }
+
+    public int getMoves() {
+        return moves;
+    }
+
+    private MoveStrategy getNextMove(){
+        int randomIndex = random.nextInt(MoveStrategy.values().length);
+        return MoveStrategy.values()[randomIndex];
+    }
+
+    public void move(){
+        MoveStrategy moveStrategy = getNextMove();
+        this.position = moveStrategy.getApplication().execute(position, gameRow, gameColumn);
+    }
 }
