26878c9adb1860689f54ca09a1a32606646cea0b
dumpierre <dumpierre@lendingclub.com>
2025-03-26 17:13:14 -0300

    WIP DPK16 -  implementation 3 Exterminator

---
+++ simple-engineering-problems/src/main/java/com/example/problems/dpk16_mosquito_game/impl3/Exterminator.java
@@ -0,0 +1,46 @@
+package com.example.problems.dpk16_mosquito_game.impl3;
+
+
+public class Exterminator {
+    private int round;
+    public int[] position;
+    private MoveStrategy moveStrategy = MoveStrategy.RIGHT;
+    private final int gameRow;
+    private final int gameColumn;
+
+    public Exterminator(int[] position, int gameRow, int gameColumn) {
+        this.position = position;
+        this.gameRow = gameRow;
+        this.gameColumn = gameColumn;
+    }
+
+
+    public void setRound(int round) {
+        this.round = round;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    public void move() {
+        if (moveStrategy == MoveStrategy.RIGHT && position[1] + 1 > gameColumn - 1 && position[0] == 0) {
+            position = MoveStrategy.RIGHT.getApplication().execute(position, gameRow, gameColumn);
+            position = MoveStrategy.UP.getApplication().execute(position, gameRow, gameColumn);
+            return;
+        }
+
+        if (moveStrategy == MoveStrategy.LEFT && position[1] - 1 < 0) {
+            moveStrategy = MoveStrategy.RIGHT;
+            position = MoveStrategy.UP.getApplication().execute(position, gameRow, gameColumn);
+            return;
+        }
+
+        position = moveStrategy.getApplication().execute(position, gameRow, gameColumn);
+
+    }
+}
