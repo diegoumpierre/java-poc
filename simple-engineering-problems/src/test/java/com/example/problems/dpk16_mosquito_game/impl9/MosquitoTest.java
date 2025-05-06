06195d28b93d1388df497d681a339203093f4201
dumpierre <dumpierre@lendingclub.com>
2025-05-06 15:49:15 -0300

    WIP DPK16 impl9

---
+++ simple-engineering-problems/src/test/java/com/example/problems/dpk16_mosquito_game/impl9/MosquitoTest.java
@@ -0,0 +1,32 @@
+package com.example.problems.dpk16_mosquito_game.impl9;
+
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+class MosquitoTest {
+
+
+    @Test
+    void moveMosquitoShouldBeSuccess() {
+        //give
+        Random random = Mockito.mock(Random.class);
+        when(random.nextInt(MoveStrategy.values().length)).thenReturn(1);
+        Mosquito mosquito = new Mosquito(random, new int[]{0, 0}, 10, 10,1);
+
+        //method under test
+        mosquito.move();
+
+        //then
+        assertArrayEquals(new int[]{1, 0}, mosquito.getPosition());
+        assertEquals(0, mosquito.getMoves());
+    }
+    
+
+}
