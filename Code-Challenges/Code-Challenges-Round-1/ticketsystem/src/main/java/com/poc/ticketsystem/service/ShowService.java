07500968143ec0dda57ff960e97df12516bc0075
dumpierre <dumpierre@lendingclub.com>
2025-05-29 18:25:44 -0300

    wip test on poc

--- Code-Challenges/Code-Challenges-Round-1/ticketsystem/src/main/java/com/poc/ticketsystem/service/ShowService.java
+++ Code-Challenges/Code-Challenges-Round-1/ticketsystem/src/main/java/com/poc/ticketsystem/service/ShowService.java
@@ -1,12 +1,17 @@
 package com.poc.ticketsystem.service;
 
+import com.poc.ticketsystem.dto.ShowSelected;
 import com.poc.ticketsystem.model.Show;
 import com.poc.ticketsystem.model.User;
 
+import java.util.List;
+
 public interface ShowService {
 
+    List<Show> listAllShow();
 
-    boolean buyTicket(User user, Show show);
+    boolean buyTicket(User user, ShowSelected showSelected);
+
 
 
 }
