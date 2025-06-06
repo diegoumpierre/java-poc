8f3725027cbdfc306bf95420e1637c2e17f42156
dumpierre <dumpierre@lendingclub.com>
2025-06-06 18:01:42 -0300

    wip test on poc

--- Code-Challenges/Code-Challenges-Round-1/ticketsystem/src/main/java/com/poc/ticketsystem/model/Venue.java
+++
@@ -1,8 +0,0 @@
-package com.poc.ticketsystem.model;
-
-import java.util.List;
-
-public class Venue {
-    private List<Zone> zoneList;
-    private String address;
-}
--- Code-Challenges/Code-Challenges-Round-1/ticketsystem/src/main/java/com/poc/ticketsystem/model/Zone.java
+++
@@ -1,9 +0,0 @@
-package com.poc.ticketsystem.model;
-
-import java.util.List;
-
-public class Zone {
-    private Venue venue;
-    private List<Seat> seatList;
-    private String name;
-}
