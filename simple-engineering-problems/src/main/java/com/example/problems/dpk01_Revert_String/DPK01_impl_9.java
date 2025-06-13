d9418b765dc67f54d44da260a1f3b2710718b208
dumpierre <dumpierre@lendingclub.com>
2025-06-13 18:49:28 -0300

    WIP

--- simple-engineering-problems/src/main/java/com/example/problems/dpk01_Revert_String/DPK01_impl_9.java
+++ simple-engineering-problems/src/main/java/com/example/problems/dpk01_Revert_String/DPK01_impl_9.java
@@ -2,12 +2,14 @@ package com.example.problems.dpk01_Revert_String;
 
 public class DPK01_impl_9 {
 
-    public static String revert(String input){
-        if (null == input) return null;
-
+    public String revert(String input){
+        if (null == input){
+            return null;
+        }
         StringBuilder result = new StringBuilder();
-        for (int i=input.length()-1;i >=0;i--){
-            result.append(input.toCharArray()[i]);
+        char[] chars = input.toCharArray();
+        for(int i=input.length()-1; i >= 0; i--){
+            result.append(chars[i]);
         }
         return result.toString();
     }
