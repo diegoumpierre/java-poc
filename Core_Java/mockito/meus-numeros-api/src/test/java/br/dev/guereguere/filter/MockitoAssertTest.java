4b1acda38d5c0caa4a2bcd73765822433707b343
Diego Umpierre <diego@umpierre.com.br>
2025-06-15 16:03:08 -0300

    mockito add the spy

--- Core_Java/mockito/meus-numeros-api/src/test/java/br/dev/guereguere/filter/MockitoAssertTest.java
+++ Core_Java/mockito/meus-numeros-api/src/test/java/br/dev/guereguere/filter/MockitoAssertTest.java
@@ -3,6 +3,7 @@ package br.dev.guereguere.filter;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,6 +11,7 @@ import static org.mockito.Mockito.when;
 
 class MockitoAssertTest {
 
+    //mock
     @Test
     void mockListSize() {
         List<String> mockedList = Mockito.mock(List.class);
@@ -17,4 +19,21 @@ class MockitoAssertTest {
 
         assertThat(mockedList.size()).isEqualTo(5);
     }
+
+    //spy
+    @Test
+    void mockListSpy(){
+        List<String> realList = new ArrayList<>();
+        List<String> spyList = Mockito.spy(realList);
+
+        spyList.add("one item");
+        spyList.add("second item");
+
+        Mockito.verify(spyList).add("one item");
+
+    }
+
+
+
+
 }
