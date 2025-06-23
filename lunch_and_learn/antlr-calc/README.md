24e01c523cb053952242ee909187e39f4b671eb5
Diego Umpierre <diego@umpierre.com.br>
2025-06-22 22:34:43 -0300

    wip in antlr

--- lunch_and_learn/antlr-calc/README.md
+++
@@ -1,5 +0,0 @@
-# Solution
-
-Slot 2 - 5 min - LT :  ANTLR - Creating a  custom DSL with Compiler
-
-Jun 26, 2025
--- lunch_and_learn/antlr-calc/src/test/java/com/poc/antlrcalc/service/CalcServiceTest.java
+++
@@ -1,7 +0,0 @@
-package com.poc.antlrcalc.service;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CalcServiceTest {
-
-}
