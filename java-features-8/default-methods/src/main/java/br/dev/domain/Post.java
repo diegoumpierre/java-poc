62fd00f687247eac86f306cfafeb30f71a69bb8f
Diego Umpierre <diego@umpierre.com.br>
2025-07-06 23:53:53 -0300

    java 8 features - add jacoco

--- java-features-8/default-methods/src/main/java/br/dev/domain/Post.java
+++ java-features-8/default-methods/src/main/java/br/dev/domain/Post.java
@@ -1,12 +1,32 @@
 package br.dev.domain;
 
-import java.time.LocalDateTime;
-
 public class Post {
-    private Long id;
     private String title;
     private String content;
-    private LocalDateTime publishedAt;
 
     private User author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
 }
--- java-features-8/default-methods/src/main/java/br/dev/interfaces/PostInterface.java
+++ java-features-8/default-methods/src/main/java/br/dev/interfaces/PostInterface.java
@@ -1,20 +1,22 @@
 package br.dev.interfaces;
 
 import br.dev.domain.Post;
+import br.dev.domain.User;
 
 public interface PostInterface {
 
     default Post getPostById(Long id) {
         System.out.println("Fetching post with ID " + id);
-        return null; // Placeholder return value
+        return null;
     }
 
-   default Post addPostToUser(Long userId, Long postId) {
-       Post post = getPostById(postId);
-         if (post == null) {
-              throw new RuntimeException("Post with ID " + postId + " not found");
-         }
-         return post;
+    default Post addPost(Long userId, String title, String content) {
+        System.out.println("Adding post for user ID " + userId + " CLASS: " + this.getClass().getSimpleName());
+        Post post = new Post();
+        post.setTitle(title);
+        post.setContent(content);
+        post.setAuthor(new User());
+        return post;
     }
 
 }
