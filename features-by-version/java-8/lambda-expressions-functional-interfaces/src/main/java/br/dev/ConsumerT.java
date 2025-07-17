0c213fce9517f2198ede6dffdc166ce01adcd267
Diego Umpierre <diego@umpierre.com.br>
2025-07-16 22:41:53 -0300

    add the examples to java 8 functional Interface usage more examples for each one, add the Consumer

--- features-by-version/java-8/lambda-expressions-functional-interfaces/src/main/java/br/dev/ConsumerT.java
+++ features-by-version/java-8/lambda-expressions-functional-interfaces/src/main/java/br/dev/ConsumerT.java
@@ -1,4 +1,51 @@
 package br.dev;
 
+import br.dev.domain.DataService;
+import br.dev.domain.Post;
+import br.dev.domain.User;
+
+import java.util.List;
+import java.util.function.Consumer;
+
 public class ConsumerT {
+
+    public static void main(String[] args) {
+        //get a user list from DataService
+        DataService dataService = new DataService();
+        List<User> userList = dataService.getUserWithPost();
+
+        // Consumer<T>: Print user info
+        Consumer<User> printUser = user -> System.out.println("User: " + user.getName() + ", Number of posts: " + user.getPosts().size());
+        userList.forEach(printUser);
+
+        // Print user name
+        Consumer<User> printUserName = user -> System.out.println("User: " + user.getName());
+        userList.forEach(user -> printUserName.accept(user));
+
+
+        // Print each post's title
+        Consumer<User> printPostTitles = user -> user.getPosts().forEach(post -> System.out.println("Title: " + post.getTitle()));
+        userList.forEach(user -> printPostTitles.accept(user));
+
+        // Print post summary (title + first 15 characters of content)
+        Consumer<Post> printSummary = post -> {
+            String summary = post.getContent().length() > 15 ? post.getContent().substring(0, 15) + "..." : post.getContent();
+            System.out.println(post.getTitle() + ": " + summary);
+        };
+        userList.forEach(user -> user.getPosts().forEach(post -> printSummary.accept(post)));
+
+
+        // Modify post content to add a tag
+        Consumer<Post> addTagToContent = post -> post.setContent(post.getContent() + " #Java8");
+        userList.forEach(user -> user.getPosts().forEach(addTagToContent));
+
+        //Print posts after modification
+        System.out.println("\nAfter tagging:");
+        userList.forEach(user -> user.getPosts().forEach(addTagToContent));
+        userList.forEach(user -> user.getPosts().forEach(post -> printSummary.accept(post)));
+
+        // Chain Consumers (print name then titles)
+        Consumer<User> printAll = printUserName.andThen(printPostTitles);
+        userList.forEach(user -> printAll.accept(user));
+    }
 }
