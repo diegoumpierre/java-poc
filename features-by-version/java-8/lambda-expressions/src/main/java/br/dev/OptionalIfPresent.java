776ebfd5676c23ea6e40482291212966a9ef42eb
Diego Umpierre <diego@umpierre.com.br>
2025-07-27 11:27:48 -0700

    understanding GroupingWithCollectors

---
+++ features-by-version/java-8/lambda-expressions/src/main/java/br/dev/OptionalIfPresent.java
@@ -0,0 +1,35 @@
+package br.dev;
+
+import br.dev.domain.DataService;
+import br.dev.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalIfPresent {
+
+
+    public static void main(String[] args) {
+        //get a user list from DataService
+        DataService dataService = new DataService();
+        List<User> userList = dataService.getUserWithPost();
+
+
+        //test optional
+        Optional<List<User>> optionalUsers = Optional.ofNullable(userList);
+        optionalUsers.ifPresent(users ->
+                users.forEach(
+                        user -> System.out.println(user.getName()))
+        );
+
+
+        //Using Optional.ifPresent with a single User
+        Optional<User> optionalUser = Optional.ofNullable(userList.get(0));
+        optionalUser.ifPresent(user -> System.out.println(user.getEmail()));
+
+
+
+    }
+
+
+}
