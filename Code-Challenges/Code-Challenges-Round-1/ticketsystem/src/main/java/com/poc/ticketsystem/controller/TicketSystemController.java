e6f207b6203dc1e9ae9c65048fb8e6c49e5cd574
dumpierre <dumpierre@lendingclub.com>
2025-09-17 20:03:30 -0700

    wip

--- Code-Challenges/Code-Challenges-Round-1/ticketsystem/src/main/java/com/poc/ticketsystem/controller/TicketSystemController.java
+++ Code-Challenges/Code-Challenges-Round-1/ticketsystem/src/main/java/com/poc/ticketsystem/controller/TicketSystemController.java
@@ -2,6 +2,7 @@ package com.poc.ticketsystem.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,12 @@ public class TicketSystemController {
     @GetMapping(value = "/")
     public ResponseEntity<String> findById(){
         return ResponseEntity.ok("welcome");
+    }
+
+
+    @PostMapping(value = "/")
+    public ResponseEntity<String> makeAReservation(){
+        return ResponseEntity.ok("reservation");
     }
 
 }
