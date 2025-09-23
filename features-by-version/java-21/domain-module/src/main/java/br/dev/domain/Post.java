13cdb2c7ba1f1668f09d2e0d1f14b58aa8223d51
Diego Umpierre <diego@umpierre.com.br>
2025-09-23 10:39:46 -0300

    Java - 21

---
+++ features-by-version/java-21/domain-module/src/main/java/br/dev/domain/Post.java
@@ -0,0 +1,37 @@
+package br.dev.domain;
+
+public class Post {
+    private String title;
+    private String content;
+    private boolean published;
+
+    public Post(String title, String content, boolean published) {
+        this.title = title;
+        this.content = content;
+        this.published = published;
+    }
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.published = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isPublished(){return this.published;}
+}
