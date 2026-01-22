d476a80dbeb0c3a82b64b2fdbcb8c6f581085373
Diego Umpierre <diego@umpierre.com.br>
2026-01-21 21:37:28 -0300

    configure auth storage client

---
+++ auth-service/src/main/java/com/poc/auth/config/AsyncConfig.java
@@ -0,0 +1,29 @@
+package com.poc.auth.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+@Slf4j
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        log.info("Creating Async Task Executor");
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-audit-");
+        executor.initialize();
+
+        return executor;
+    }
+}
