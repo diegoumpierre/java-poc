5ef5b745d4dda206cd899156ad03658757af82bd
Diego Umpierre <diego@umpierre.com.br>
2026-02-24 19:13:54 -0300

    setup chat websocket config

---
+++ chat-service/src/main/java/com/poc/chat/config/JdbcConfig.java
@@ -0,0 +1,58 @@
+package com.poc.chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
+import org.springframework.data.jdbc.core.convert.JdbcCustomConversions;
+import org.springframework.lang.NonNull;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+@Configuration
+public class JdbcConfig {
+
+    @Bean
+    public JdbcCustomConversions jdbcCustomConversions() {
+        return new JdbcCustomConversions(Arrays.asList(
+            new UuidToStringConverter(),
+            new StringToUuidConverter(),
+            new IntegerToBooleanConverter(),
+            new BooleanToIntegerConverter()
+        ));
+    }
+
+    @WritingConverter
+    static class UuidToStringConverter implements Converter<UUID, String> {
+        @Override
+        public String convert(@NonNull UUID source) {
+            return source.toString();
+        }
+    }
+
+    @ReadingConverter
+    static class StringToUuidConverter implements Converter<String, UUID> {
+        @Override
+        public UUID convert(@NonNull String source) {
+            return UUID.fromString(source);
+        }
+    }
+
+    @ReadingConverter
+    static class IntegerToBooleanConverter implements Converter<Integer, Boolean> {
+        @Override
+        public Boolean convert(@NonNull Integer source) {
+            return source != 0;
+        }
+    }
+
+    @WritingConverter
+    static class BooleanToIntegerConverter implements Converter<Boolean, Integer> {
+        @Override
+        public Integer convert(@NonNull Boolean source) {
+            return source ? 1 : 0;
+        }
+    }
+}
